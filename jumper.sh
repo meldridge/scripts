8608cc04a0cff5083b078fc374147ed4027e011b
Mark Eldridge <mark.d.eldridge@gmail.com>
2017-05-02 12:01:50 +0930

    Updated jumper script for AWS

--- jumper.sh
+++ jumper.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Connect to jumper and set up remote forwarding for ports 80 and 443
-ssh -t root@jumper -R 40080:localhost:80 -R 48080:localhost:8080 -R 40443:localhost:443 -R 48443:localhost:8443 -R 44444:localhost:4444 'service rinetd start; bash -l'
-ssh root@jumper 'service rinetd stop'
+ssh -t ubuntu@jumper -i ~/.ssh/JumperAWS.pem -R 40080:localhost:80 -R 48080:localhost:8080 -R 40443:localhost:443 -R 48443:localhost:8443 -R 44444:localhost:4444 'sudo service rinetd start; bash -l'
+ssh ubuntu@jumper -i ~/.ssh/JumperAWS.pem 'sudo service rinetd stop'
