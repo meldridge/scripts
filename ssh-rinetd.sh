579abd4abfffa904ac4cdc7aa9fc9d74af5020f0
Mark Eldridge <mark@kali>
2018-05-06 20:00:27 +0200

    Added ssh-* scripts

--- ssh-rinetd.sh
+++ ssh-rinetd.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 # Connect to proxy server and set up remote forwarding with rinetd
-ssh -t mark@proxy -R 40443:localhost:443 'sudo service rinetd start; bash -l'
+ssh -t mark@proxy \
+  -R 40080:localhost:80 \
+  -R 40443:localhost:443 \
+  -R 48080:localhost:8080 \
+  -R 48443:localhost:8443 \
+  -R 41194:localhost:1194 \
+  'sudo service rinetd start; bash -l'
 ssh mark@proxy 'sudo service rinetd stop'
