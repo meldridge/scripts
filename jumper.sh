84e8b58b6358e4a2f7bd7a786722a1c59685b7c3
Mark Eldridge <8285554+meldridge@users.noreply.github.com>
2018-05-20 22:45:10 +0200

    Updated to use GatewayPorts rather than rinetd

--- jumper.sh
+++ jumper.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# Connect to jumper and set up remote forwarding
-ssh -t mark@jumper -R 40080:localhost:80 -R 40443:localhost:443 -R 48080:localhost:8080 -R 48443:localhost:8443 -R 44444:localhost:4444 -R 41194:localhost:1194 'sudo service rinetd start; bash -l'
-ssh mark@jumper 'sudo service rinetd stop'
+# Connect to jumper and set up remote forwarding for ports 80, 443, 8080, 8443, and 1194
+ssh root@jumper -R 80:localhost:80 -R 443:localhost:443 -R 8080:localhost:8080 -R 8443:localhost:8443 -R 1194:localhost:1194
