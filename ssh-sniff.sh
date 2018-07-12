f311950e1b7d611ecb71564fadd1311a4f487b9a
Mark Eldridge <8285554+meldridge@users.noreply.github.com>
2018-07-12 12:26:35 +0200

    Update ssh-sniff.sh

--- ssh-sniff.sh
+++ ssh-sniff.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # Tries to sniff incoming SSH connections
+# MUST BE RUN AS ROOT!
 if [ -z "$1" ]; then
         echo "Usage: ./ssh-sniff.sh <username>"
         exit 0
@@ -16,10 +17,17 @@ then
         echo "Check manually using ps (it may have a different command line)"
         exit 0
 fi
-        
+
 echo "Waiting for ssh connections from $USERNAME..."
 echo "Ctrl+C to exit"
 
 # Trace the sshd process for write events, and grep for the username
+# If this fails, it's probably because someone else is also tracing sshd
 # NOTE: grep context may need tweaking
 strace -f -p $PID -e write 2>&1 | grep -B 2 -A 10 $USERNAME
+
+if [ $? -eq 1 ]
+then
+  echo "Couldn't trace sshd process!"
+  echo "Either someone else is already running strace, or ours was killed."
+fi
